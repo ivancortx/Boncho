19b86cf643f76d0ef1e3af9855a73eaa0b61bb0e
Ivan Melnychuk <ivancortx@gmail.com>
2021-11-06 21:03:01 +0200

    add logo

--- src/ui/navigation/components/LogoAndNavButtons/LogoAndNavButtons.tsx
+++ src/ui/navigation/components/LogoAndNavButtons/LogoAndNavButtons.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
-import logo from 'assets/images/HomePage/Logo.svg'
+import logo from 'assets/images/logo_boncho.png'
 import {Routes} from '../../const/routes'
 import {Button} from '../Button/Button'
 
 import styles from './LogoAndNavButtons.module.scss'
 
-export const LogoAndNavButtons: React.VFC = () => (
-    <div className={styles.logoAndNavButtons}>
+export const LogoAndNavButtons: React.VFC = () => {
+
+  return (
+  <div className={styles.logoAndNavButtons}>
+    <Link to={Routes.Home}>
       <img className={styles.logo} src={logo} alt={'logo'}/>
-      <div className={styles.navButtonsContainer}>
-        <div className={styles.button}>
-          <Link to={Routes.Home}><Button text={'Главная'}/></Link>
-        </div>
-        <div className={styles.button}>
-          <Link to={Routes.AuctionsPage}><Button text={'Аукционы'}/></Link>
-        </div>
-        <div className={styles.button}>
-          <Link to={Routes.Products}><Button text={'Категории'}/></Link>
-        </div>
-        <div className={styles.button}>
-          <Link to={Routes.AddProductPage}><Button text={'Добавить аукцион'}/></Link>
-        </div>
+    </Link>
+
+    <div className={styles.navButtonsContainer}>
+      <div className={styles.button}>
+        <Link to={Routes.Home}><Button text={'Главная'}/></Link>
+      </div>
+      <div className={styles.button}>
+        <Link to={Routes.AuctionsPage}><Button text={'Аукционы'}/></Link>
+      </div>
+      <div className={styles.button}>
+        <Link to={Routes.Products}><Button text={'Категории'}/></Link>
+      </div>
+      <div className={styles.button}>
+        <Link to={Routes.AddProductPage}><Button text={'Добавить аукцион'}/></Link>
       </div>
     </div>
-)
+  </div>)
+}
